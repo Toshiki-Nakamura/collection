b255b2640cb00d37e4f5c15f07f088140ee8bfa9
Toshiki　Nakamura <t.jack0624@gmail.com>
2021-09-01 14:24:19 +0900

    split

--- split.cpp
+++ split.cpp
@@ -14,24 +14,21 @@ bool contain(const std::basic_string<T>& s, const U& v)
 vector<string > split(string str, string sep)
 {
 	vector<string > vec;
-	size_t len = str.length();
 	size_t seplen = sep.length();
+	size_t pos = 0, i = 0;
 
 	if (contain(str, sep) == false)
 	{
 		vec.push_back(str);
 		return vec;
 	}
-	size_t pos = 0;
-	size_t i = 0;
 	while (contain(str, sep))
 	{
 		pos = str.find(sep);
 		string s = str.substr(0, pos); //str[0]~str[pos-1]
+		if (s != "") vec.push_back(s);
 		i = pos + seplen;
 		str = str.substr(i, str.size() - i); // str[i]~str[len-1]
-		if (s != "") vec.push_back(s);
-		// cout << s << endl;
 	}
 	if (str.size())
 		vec.push_back(str);
